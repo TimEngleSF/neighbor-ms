4d0e401c84abf1e7c85ad7fa0cae7f443f987449
Tim Engle <timothy.englew@gmail.com>
2024-06-13 13:23:29 -0700

    add vars for img dirs

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ CLIENT_KEY = os.getenv("CLIENT_KEY")
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 WEBHOOK_KEY = os.getenv("WEBHOOK_KEY")
 LEONARDO_KEY = os.getenv("LEONARDO_KEY")
+
+PREPROCESSED_IMG_DIR = "static/preprocessed"
+PROCESSED_IMG_DIR = "static/processed"
